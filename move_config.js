422e7ccdcd13047994c07181208e2820cedd8b97
折影轻梦 <nexmoe@qq.com>
2021-06-12 11:53:13 +0800

    #7

---
+++ move_config.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+if (fs.existsSync('../hexo/package.json')){
+  const version = JSON.parse(fs.readFileSync('../hexo/package.json')).version
+  if (version.split('.')[0] === '5') {
+    const oldConfigPath = '../../_config.old.yml'
+    const configPath = '../../_config.nexmoe.yml'
+    if (!fs.existsSync(configPath) && !fs.existsSync(oldConfigPath)) {
+      fs.writeFileSync(configPath, fs.readFileSync('./_config.yml'))
+    }
+    if (fs.existsSync(configPath)){
+      fs.writeFileSync( '../../_config.new.yml', fs.readFileSync('./_config.yml'))
+    }
+    fs.unlinkSync('./_config.yml')
+  }
+}
+
+// 借鉴自 https://github.com/HCLonely/hexo-theme-webstack/blob/master/move_config.js
