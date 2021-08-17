ad16968cde209f930bb9f1a8a68aac3a4a7e8f52
折影轻梦 <nexmoe@qq.com>
2021-08-17 09:52:01 +0800

    归档页，设置默认显示的分类，其他隐藏

--- source/js/core.js
+++ source/js/core.js
@@ -1,6 +1,24 @@
 function search() {
 	window.open(
-        document.getElementById("search_form").getAttribute("action_e") + " " + document.getElementById("search_value").value
+		document.getElementById("search_form").getAttribute("action_e") +
+			" " +
+			document.getElementById("search_value").value
 	);
 	return false;
+}
+
+let display = false;
+function displayAll() {
+	let elements = document.querySelectorAll(".catalogue .hidden");
+	if (!display) {
+		for (let elem of elements) {
+			elem.style.display = "block";
+			display = true;
+		}
+	} else {
+		for (let elem of elements) {
+			elem.style.display = "none";
+			display = false;
+		}
+	}
 }
